effda770bcd08f8aa0102ebde12bfb4562c59cc2
Paul K <crtlib@gmail.com>
2018-06-09 16:40:39 +0200

    loki logos

--- app/src/main/java/com/m2049r/xmrwallet/ReceiveFragment.java
+++ app/src/main/java/com/m2049r/xmrwallet/ReceiveFragment.java
@@ -387,7 +387,7 @@ public class ReceiveFragment extends Fragment {
 
     private Bitmap getMoneroLogo() {
         if (logo == null) {
-            logo = Helper.getBitmap(getContext(), R.drawable.ic_monero_logo_b);
+            logo = Helper.getBitmap(getContext(), R.drawable.ic_loki_logo_b);
         }
         return logo;
     }
--- app/src/main/java/com/m2049r/xmrwallet/service/WalletService.java
+++ app/src/main/java/com/m2049r/xmrwallet/service/WalletService.java
@@ -550,7 +550,7 @@ public class WalletService extends Service {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
         Notification notification = new Notification.Builder(this)
                 .setContentTitle(getString(R.string.service_description))
-                .setSmallIcon(R.drawable.ic_monerujo)
+                .setSmallIcon(R.drawable.loki_notification)
                 .setContentIntent(pendingIntent)
                 .build();
         startForeground(NOTIFICATION_ID, notification);
