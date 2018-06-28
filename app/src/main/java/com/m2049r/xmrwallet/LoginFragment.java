d16cee97d89f05750e5c8d4cf145ce373d3691b0
Paul K <crtlib@gmail.com>
2018-06-28 22:32:49 +0200

    Lunther is Gunther's brother updated horizontal logo in toolbar

--- app/src/main/java/com/m2049r/xmrwallet/LoginFragment.java
+++ app/src/main/java/com/m2049r/xmrwallet/LoginFragment.java
@@ -287,7 +287,7 @@ public class LoginFragment extends Fragment implements WalletInfoAdapter.OnInter
         if (displayedList.isEmpty()) {
             fab.startAnimation(fab_pulse);
             if (ivGunther.getDrawable() == null) {
-                ivGunther.setImageResource(R.drawable.gunther_desaturated);
+                ivGunther.setImageResource(R.drawable.lunther_big);
             }
         } else {
             fab.clearAnimation();
